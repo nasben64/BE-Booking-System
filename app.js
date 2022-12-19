58f6ea5be46ef08be55737f2cefa02d5f40e8569
Gary Shaw <garyshaw05@gmail.com>
2022-12-19 16:50:22 +0000

    MVC set-up in progress

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const routes = require("./routes/routes.js");
+const { getAppointments } = require("./controllers/appointments");
+
+const app = express();
+app.use(express.json());
+
+// app.use("/api", routes);
+
+app.get("/api/appointments", getAppointments);
+
+module.exports = app;
