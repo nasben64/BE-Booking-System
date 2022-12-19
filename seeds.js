a7368ffb6a6cc2e1a23a9f5e5e8a4ff8645d0209
Gary Shaw <garyshaw05@gmail.com>
2022-12-19 15:25:32 +0000

    user schema created & seeded

--- seeds.js
+++ seeds.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 const Appointments = require("./models/appointments.js");
+const Users = require("./models/users.js");
 
 const mongoString = process.env.DATABASE_URL;
 mongoose.connect(mongoString);
@@ -47,11 +48,76 @@ const seedAppointments = [
     date: "2022-12-19",
     time: "16:00",
   },
+  {
+    date: "2022-12-20",
+    time: "09:00",
+  },
+  {
+    date: "2022-12-20",
+    time: "10:00",
+  },
+  {
+    date: "2022-12-20",
+    time: "11:00",
+  },
+  {
+    date: "2022-12-20",
+    time: "12:00",
+  },
+  {
+    date: "2022-12-20",
+    time: "13:00",
+  },
+  {
+    date: "2022-12-20",
+    time: "14:00",
+  },
+  {
+    date: "2022-12-20",
+    time: "15:00",
+  },
+  {
+    date: "2022-12-20",
+    time: "16:00",
+  },
+];
+
+const seedUsers = [
+  {
+    firstName: "Gary",
+    lastName: "Shaw",
+    email: "gary.shaw@gmx.co.uk",
+    username: "gs",
+    password: "password",
+  },
+  {
+    firstName: "Nasser",
+    lastName: "Benashur",
+    email: "nasser@yahoo.com",
+    username: "nb",
+    password: "password",
+  },
+  {
+    firstName: "Robbie",
+    lastName: "McDonough",
+    email: "robbie@hotmail.com",
+    username: "rm",
+    password: "password",
+  },
+  {
+    firstName: "Ryan",
+    lastName: "Nolan",
+    email: "ryan@msn.com",
+    username: "rn",
+    password: "password",
+  },
 ];
 
 const seedDB = async () => {
   await Appointments.deleteMany({});
+  await Users.deleteMany({});
   await Appointments.insertMany(seedAppointments);
+  await Users.insertMany(seedUsers);
 };
 
 seedDB().then(() => {
